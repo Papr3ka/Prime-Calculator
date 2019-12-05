a0593ba9625d3e7415a2c51c406e4e310465912c
Benjamin <58241833+Papr3ka@users.noreply.github.com>
2019-12-05 17:06:01 -0500

    Update Prime.py

--- Prime.py
+++ Prime.py
@@ -2,8 +2,7 @@
 # Copyright (c) 2019 Benjamin Yao
 import os
 import platform
-import math
-waitsetc = 0
+waitsetc = 1
 def clear():
     if platform.system() == 'Windows':
         os.system('cls')
@@ -18,28 +17,31 @@ def wait():
     clear()
 def checkprime(x):
     if x >= 2:
-        for y in range(2,x):
+        for y in range(2,x,1):
             if not( x % y ):
                 return False
     else:
         return False
     return True
 def normprcalc():
-    prch = minprch
-    for prch in range(minprch,maxprch,1):
-        if checkprime(prch) == True:
-            print(prch)
-def lucaslehmer():
-    prch
+    primes = [2]
+    pcrh = 1
+    for prch in range(1,maxprch,2):
+        if checkprime(prch):
+            trueprime = prch
+            primes.append(trueprime)
+            print(trueprime)
+    for x in primes:
+        print(x)
 clear()
 print("Select Primality Algorithm")
-print("1:Normal Prime\n2:lucas Lehmer")
+print("1:Normal Prime")
 option = int(input("Option:"))
 clear()
-minprch = int(input("Minimum Prime:"))
 maxprch = int(input("Maximum Prime:"))
 clear()
 if option == 1:
+    wait()
     normprcalc()
-if option == 2:
-    
+waitsetc = 0
+wait()
